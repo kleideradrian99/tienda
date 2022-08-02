9bc4380835287e19cdea08a1f2fd7e9663309f8b
Adrian <kleiidermachado@gmail.com>
2022-08-02 17:32:14 -0500

    Login Admin and Cliente

--- back/controllers/ClienteController.js
+++ back/controllers/ClienteController.js
@@ -3,7 +3,7 @@
 var Cliente = require('../models/cliente');
 var bcrypt = require('bcrypt-nodejs');
 
-const registro_cliente = async function(req, res) {
+const registro_cliente = async function (req, res) {
 
     var data = req.body;
     var clientes_arr = [];
@@ -15,7 +15,7 @@ const registro_cliente = async function(req, res) {
 
 
         if (data.password) {
-            bcrypt.hash(data.password, null, null, async function(err, hash) {
+            bcrypt.hash(data.password, null, null, async function (err, hash) {
                 if (hash) {
                     data.password = hash;
                     var reg = await Cliente.create(data);
@@ -35,6 +35,29 @@ const registro_cliente = async function(req, res) {
 
 }
 
+const login_cliente = async function (req, res) {
+    var data = req.body;
+    var cliente_arr = [];
+
+    cliente_arr = await Cliente.find({ email: data.email });
+
+    if (cliente_arr.length == 0) {
+        res.status(200).send({ message: 'No se encontro el correo', data: undefined });
+    } else {
+        //LOGIN
+        let user = cliente_arr[0];
+
+        bcrypt.compare(data.password, user.password, async function (error, check) {
+            if (check) {
+                res.status(200).send({ data: user });
+            } else {
+                res.status(200).send({ message: 'La contraseña no coincide', data: undefined });
+            }
+        });
+    }
+}
+
 module.exports = {
-    registro_cliente
+    registro_cliente,
+    login_cliente
 }
