f6e56e498599450f3f2f5c95e450658574d2458c
kleider <kleidermachado@gmail.com>
2022-08-03 19:06:29 -0500

    Plantilla Crear CLientes

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { routing } from './app.routing';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
+import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { LoginComponent } from './components/login/login.component';
     InicioComponent,
     SidebarComponent,
     LoginComponent,
+    IndexClienteComponent,
   ],
   imports: [
     BrowserModule,
