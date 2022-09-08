2f53d199257f0261ffd4ad435b860cf347777400
Adrian <kleiidermachado@gmail.com>
2022-09-08 15:58:28 -0500

    Exportando productos e Inventario

--- admin/src/app/components/productos/index-productos/index-productos.component.ts
+++ admin/src/app/components/productos/index-productos/index-productos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { global } from 'src/app/services/global';
 import { ProductoService } from 'src/app/services/producto.service';
+import * as fs from 'file-saver';
+import { Workbook } from 'exceljs';
+
 
 declare var iziToast: any;
 declare var jQuery: any;
@@ -11,12 +14,14 @@ declare var $: any;
   templateUrl: './index-productos.component.html',
   styleUrls: ['./index-productos.component.css']
 })
+
 export class IndexProductosComponent implements OnInit {
 
   public load_data = true;
   public filtro = '';
   public token;
   public productos: Array<any> = [];
+  public arr_productos: Array<any> = [];
   public url;
   public load_btn = false;
 
@@ -39,6 +44,16 @@ export class IndexProductosComponent implements OnInit {
     this._productoService.listar_producto_admin(this.filtro, this.token).subscribe(
       response => {
         this.productos = response.data;
+        this.productos.forEach(element => {
+          this.arr_productos.push({
+            titulo: element.titulo,
+            stock: element.stock,
+            precio: element.precio,
+            categoria: element.categoria,
+            nventas: element.nventas,
+          });
+        });
+        console.log(this.arr_productos);
         this.load_data = false;
       }, error => {
         console.log(error);
@@ -108,6 +123,38 @@ export class IndexProductosComponent implements OnInit {
         this.load_btn = false;
       }
     )
+  }
+
+  download_excel() {
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet("Reporte de productos");
+
+    worksheet.addRow(undefined);
+    for (let x1 of this.arr_productos) {
+      let x2 = Object.keys(x1);
+
+      let temp = [];
+      for (let y of x2) {
+        temp.push(x1[y]);
+      }
+      worksheet.addRow(temp);
+    }
+
+    let fname = 'REP01- ';
+
+    worksheet.columns = [
+      { header: 'Producto', key: 'col1', width: 30 },
+      { header: 'Stock', key: 'col2', width: 15 },
+      { header: 'Precio', key: 'col3', width: 15 },
+      { header: 'Categoria', key: 'col4', width: 25 },
+      { header: 'N° ventas', key: 'col5', width: 15 },
+    ] as any;
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, fname + '-' + new Date().valueOf() + '.xlsx');
+    });
+
   }
 
 }
--- admin/src/app/components/productos/inventario-producto/inventario-producto.component.ts
+++ admin/src/app/components/productos/inventario-producto/inventario-producto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductoService } from 'src/app/services/producto.service';
+import * as fs from 'file-saver';
+import { Workbook } from 'exceljs';
 
 declare var iziToast: any;
 declare var jQuery: any;
@@ -20,6 +22,7 @@ export class InventarioProductoComponent implements OnInit {
   public inventario: any = {};
   public load_btn = false;
   public inventarios: Array<any> = [];
+  public arr_inventario: Array<any> = [];
 
   constructor(
     private _route: ActivatedRoute,
@@ -44,6 +47,13 @@ export class InventarioProductoComponent implements OnInit {
               this._productoService.listar_inventario_admin(this.producto._id, this.token).subscribe(
                 response => {
                   this.inventarios = response.data;
+                  this.inventarios.forEach(element => {
+                    this.arr_inventario.push({
+                      titulo: element.admin.nombres + ' ' + element.admin.apellidos,
+                      cantidad: element.cantidad,
+                      proveedor: element.proveedor,
+                    });
+                  });
                 }, error => {
                   console.log(error)
                 }
@@ -128,7 +138,7 @@ export class InventarioProductoComponent implements OnInit {
               console.log(error)
             }
           )
-          
+
         }, error => {
           console.log(error)
         }
@@ -144,6 +154,36 @@ export class InventarioProductoComponent implements OnInit {
         message: 'Los datos del formulario no son validos'
       });
     }
+  }
+
+  download_excel() {
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet("Reporte de productos");
+
+    worksheet.addRow(undefined);
+    for (let x1 of this.arr_inventario) {
+      let x2 = Object.keys(x1);
+
+      let temp = [];
+      for (let y of x2) {
+        temp.push(x1[y]);
+      }
+      worksheet.addRow(temp);
+    }
+
+    let fname = 'REP01- ';
+
+    worksheet.columns = [
+      { header: 'Trabajador', key: 'col1', width: 30 },
+      { header: 'Cantidad', key: 'col2', width: 15 },
+      { header: 'Proveedor', key: 'col3', width: 25 },
+    ] as any;
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, fname + '-' + new Date().valueOf() + '.xlsx');
+    });
+
   }
 
 }
