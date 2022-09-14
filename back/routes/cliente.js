5a8e859249c891da7a3f258a453a652b628e7f85
Adrian <kleiidermachado@gmail.com>
2022-09-14 11:32:16 -0500

    Login Tienda

--- back/routes/cliente.js
+++ back/routes/cliente.js
@@ -14,5 +14,6 @@ api.post('/registro_cliente_admin', auth.auth, clienteController.registro_client
 api.get('/obtener_cliente_admin/:id', auth.auth, clienteController.obtener_cliente_admin);
 api.put('/actualizar_cliente_admin/:id', auth.auth, clienteController.actualizar_cliente_admin);
 api.delete('/eliminar_cliente_admin/:id', auth.auth, clienteController.eliminar_cliente_admin);
+api.get('/obtener_cliente_guest/:id', auth.auth, clienteController.obtener_cliente_guest);
 
 module.exports = api;
