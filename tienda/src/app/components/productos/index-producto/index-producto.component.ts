b3c614c6f8c6ddfb8010590753f4bee10aeabbe2
Adrian <kleiidermachado@gmail.com>
2022-09-26 10:38:26 -0500

    order_por

--- tienda/src/app/components/productos/index-producto/index-producto.component.ts
+++ tienda/src/app/components/productos/index-producto/index-producto.component.ts
@@ -24,9 +24,12 @@ export class IndexProductoComponent implements OnInit {
 
   public route_categoria: any;
 
-    //PAGINACION
-    public page = 1;
-    public pageSize = 15;
+  //PAGINACION
+  public page = 1;
+  public pageSize = 15;
+
+  // Filtros paginacion
+  public sort_by = 'Defecto';
 
   constructor(
     private _clienteService: ClienteService,
@@ -157,5 +160,65 @@ export class IndexProductoComponent implements OnInit {
         this.productos = response.data;
         this.load_data = false;
       });
+  }
+
+  order_por() {
+    if (this.sort_by == 'Defecto') {
+      this._clienteService.listar_producto_publico('').subscribe(
+        response => {
+          this.productos = response.data;
+          this.load_data = false;
+        });
+    } else if (this.sort_by == 'Popularidad') {
+      this.productos.sort(function (a, b) {
+        if (a.nventas < b.nventas) {
+          return 1;
+        }
+        if (a.nventas > b.nventas) {
+          return -1;
+        }
+        return 0;
+      });
+    } else if (this.sort_by == '+-Precio') {
+      this.productos.sort(function (a, b) {
+        if (a.precio < b.precio) {
+          return 1;
+        }
+        if (a.precio > b.precio) {
+          return -1;
+        }
+        return 0;
+      });
+    } else if (this.sort_by == '-+Precio') {
+      this.productos.sort(function (a, b) {
+        if (a.precio > b.precio) {
+          return 1;
+        }
+        if (a.precio < b.precio) {
+          return -1;
+        }
+        return 0;
+      });
+    } else if (this.sort_by == 'azTitulo') {
+      this.productos.sort(function (a, b) {
+        if (a.titulo > b.titulo) {
+          return 1;
+        }
+        if (a.titulo < b.titulo) {
+          return -1;
+        }
+        return 0;
+      });
+    } else if (this.sort_by == 'zaTitulo') {
+      this.productos.sort(function (a, b) {
+        if (a.titulo < b.titulo) {
+          return 1;
+        }
+        if (a.titulo > b.titulo) {
+          return -1;
+        }
+        return 0;
+      });
+    }
   }
 }
